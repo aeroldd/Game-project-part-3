96b682c8b3465a6bb53a364fee867dbc1e69d293
aeroldd <aeroldzeidparcon@gmail.com>
2025-04-04 13:55:25 +0400

    compiles new source files

--- compile.bat
+++ compile.bat
@@ -1,3 +1,3 @@
 echo "Compiling game..."
-gcc main.c entity.c combat.c pathfinding.c room.c util.c dialogue.c map.c item.c menus.c input.c player.c -o game
+gcc main.c entity.c combat.c pathfinding.c room.c util.c dialogue.c map.c item.c menus.c input.c player.c monster.c initiative.c -o game
 echo "Game compilation finished."
